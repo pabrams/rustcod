a64c8645534784af8a3fa84e4dfcabd16533277b
Paul Abrams <john.paul.abrams@gmail.com>
2022-10-09 04:59:56 -0400

    only process key.pressed

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,25 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool 
     use tcod::input::KeyCode::*;
     
     let key = tcod.root.wait_for_keypress(true);
-    match key {
-        Key {
-            code: Enter,
-            alt: true,
-            ..
-        } => {
-            // Alt+Enter: toggle fullscreen
-            let fullscreen = tcod.root.is_fullscreen();
-            tcod.root.set_fullscreen(!fullscreen);
-        }
-        Key { code: Escape, .. } => return true, // exit game
+    if key.pressed {
+        match key {
+            Key {
+                code: Enter,
+                alt: true,
+                ..
+            } => {
+                // Alt+Enter: toggle fullscreen
+                let fullscreen = tcod.root.is_fullscreen();
+                tcod.root.set_fullscreen(!fullscreen);
+            }
+            Key { code: Escape, .. } => return true, // exit game
 
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
-        _ => {}
+            Key { code: Up, .. } => *player_y -= 1,
+            Key { code: Down, .. } => *player_y += 1,
+            Key { code: Left, .. } => *player_x -= 1,
+            Key { code: Right, .. } => *player_x += 1,
+            _ => {}
+        }
     }
     false
 }
