066d2a0db39bb42d8c8b99d061438d81484bd7d4
Paul Abrams <john.paul.abrams@gmail.com>
2022-10-27 23:45:05 -0400

    draw a dungeon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
 use tcod::console::*;
 use tcod::colors::*;
+use std::cmp;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+const LIMIT_FPS: i32 = 30;
+
+struct Tcod {
+    root: Root,
+    con: Offscreen,
+}
+
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+/// A rectangle on the map, used to characterise a room.
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Object {
@@ -23,28 +88,48 @@ impl Object {
     }
 
     pub fn draw(&self, con: &mut dyn Console) {
-        con.set_default_foreground (self.color);
+        con.set_default_foreground(self.color);
         con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
     }
 }
 
-type Map = Vec<Vec<Tile>>;
 
-struct Game {
-    map: Map,
+fn create_room(room: Rect, map: &mut Map) {
+    // go through the tiles in the rectangle and make them passable
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // horizontal tunnel. `min()` and `max()` are used in case `x1 > x2`
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // vertical tunnel
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
 }
 
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    map[30][22] = Tile::wall();
-    map[50][24] = Tile::wall();
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    // create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
     map
 }
 
 fn render(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
-    for object in objects {
-        object.draw(&mut tcod.con);
-    }
 
     // go through all tiles, and set their background color
     for y in 0..MAP_HEIGHT {
@@ -60,6 +145,10 @@ fn render(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
         }
     }
 
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
     // blit the contents of "con" to the root console and present it
     blit(
         &tcod.con,
@@ -72,73 +161,37 @@ fn render(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
     );
 }
 
-// actual size of the window
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
-
-const MAP_WIDTH: i32 = 80;
-const MAP_HEIGHT: i32 = 45;
-
-const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
-const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
-
-#[derive(Clone, Copy, Debug)]
-struct Tile {
-    blocked: bool,
-    block_sight: bool,
-}
-
-impl Tile {
-    pub fn empty() -> Self {
-        Tile {
-            blocked: false,
-            block_sight: false,
-        }
-    }
-
-    pub fn wall() -> Self {
-        Tile {
-            blocked: true,
-            block_sight: true,
-        }
-    }
-}
-const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
-struct Tcod {
-    root: Root,
-    con: Offscreen,
-}
-
 fn handle_keys(tcod: &mut Tcod, player: &mut Object, game: &Game) -> bool {
-
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     
     let key = tcod.root.wait_for_keypress(true);
-    if key.pressed {
-        match key {
-            Key {
-                code: Enter,
-                alt: true,
-                ..
-            } => {
-                // Alt+Enter: toggle fullscreen
-                let fullscreen = tcod.root.is_fullscreen();
-                tcod.root.set_fullscreen(!fullscreen);
-            }
-            Key { code: Escape, .. } => return true, // exit game
-
-            Key { code: Up, .. } => player.move_by(0, -1, game),
-            Key { code: Down, .. } => player.move_by(0, 1, game),
-            Key { code: Left, .. } => player.move_by(-1, 0, game),
-            Key { code: Right, .. } => player.move_by(1, 0, game),
-            _ => {}
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
         }
+        Key { code: Escape, .. } => return true, // exit game
+
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
+        _ => {}
     }
     false
 }
 
+
 fn main() {
+
+    tcod::system::set_fps(LIMIT_FPS);
+
     let root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
@@ -148,12 +201,11 @@ fn main() {
         
     let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut tcod = Tcod { root, con };
-    tcod::system::set_fps(LIMIT_FPS);
 
     const START_X: i32 = SCREEN_WIDTH / 2;
     const START_Y: i32 = SCREEN_HEIGHT / 2;
 
-    let player = Object::new(START_X, START_Y, '@', WHITE);
+    let player = Object::new(25, 23, '@', WHITE);
     let npc = Object::new(START_X - 5, START_Y, '@', YELLOW);
 
     let mut objects = [player, npc];
@@ -166,9 +218,8 @@ fn main() {
         tcod.con.clear();
         render(&mut tcod, &game, &objects);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
-
         let player = &mut objects[0];
+
         // handle keys and exit game if needed
         let exit = handle_keys(&mut tcod, player, &game);
         if exit {
